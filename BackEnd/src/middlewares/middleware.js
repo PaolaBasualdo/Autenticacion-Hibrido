04a38806ffa90bb9c0ed870c582662df4a8ac51c
GiseCristaldo <giselle.cristaldo.1994@gmail.com>
2025-09-03 20:48:23 -0300

    elimino la carpeta middleware plural

--- BackEnd/src/middlewares/middleware.js
+++
@@ -1,6 +0,0 @@
-// Middleware de ejemplo para proteger rutas (puedes personalizarlo)
-
-export const ejemploMiddleware = (req, res, next) => {
-  // Aquí puedes poner lógica de autenticación, logging, etc.
-  next();
-};
